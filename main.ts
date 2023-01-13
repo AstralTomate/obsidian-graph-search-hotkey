c7fd88f5be6634a290660028b658bcdf4a36d9c3
Johannes Geisler <johgeisler@gmail.com>
2023-01-13 13:29:35 +0100

    Fix using the depricated activeLeaf class

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Plugin} from 'obsidian';
+import { App, Plugin, View} from 'obsidian';
 
 
 export default class EasyGraphSearch extends Plugin {
@@ -10,19 +10,26 @@ export default class EasyGraphSearch extends Plugin {
 			name: 'Simply jump to the search bar in Graph view',
 			hotkeys: [{ modifiers: ["Alt"], key: "f" }],
 			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const graphView = this.app.workspace.getLeaf().getViewState().type;	
-								
-				if (graphView == "graph") {	
-					// make sure that the graph controls is visable
-					this.app.workspace.activeLeaf.view.dataEngine.controlsEl.getElementsByClassName("clickable-icon graph-controls-button mod-open")[0].click()
-					// make sure that the filter options are not collapsed
-					this.app.workspace.activeLeaf.view.dataEngine.filterOptions.setCollapsed(false)
-					// select the input field
-					this.app.workspace.activeLeaf.view.dataEngine.filterOptions.search.inputEl.select(true)
-					// Put the cursor in the end of the input field
-					this.app.workspace.activeLeaf.view.dataEngine.filterOptions.search.inputEl.setSelectionRange(this.app.workspace.activeLeaf.view.dataEngine.filterOptions.search.inputEl.selectionEnd,this.app.workspace.activeLeaf.view.dataEngine.filterOptions.search.inputEl.selectionEnd)
+				// Conditions to check				
+				const graphView = this.app.workspace.getActiveViewOfType(View);
+				console.log(graphView);
+				// Check if null
+				if (graphView) {
+					// Check if the active view is a graph
+					if (graphView.getViewType() == "graph") {	
+						// make sure that the graph controls is visable
+						graphView.dataEngine.controlsEl.getElementsByClassName("clickable-icon graph-controls-button mod-open")[0].click()
+						// make sure that the filter options are not collapsed
+						graphView.dataEngine.filterOptions.setCollapsed(false)
+						// define the searchbox element
+						const searchBox = graphView.dataEngine.filterOptions.search.inputEl;
+						// select the input field TODO: copy search to clipboard? or add to settings
+						searchBox.select(true)
+						// Put the cursor in the end of the input field
+						searchBox.setSelectionRange(searchBox.selectionEnd, searchBox.selectionEnd)
+					}
 				}
+				
 			}
 		});
 
